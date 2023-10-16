3647ba82be29ee049618ba278b87c897280f71b5
Quinn Palmer <Quinn@milyli.net>
2023-10-16 10:06:51 -0500

    Feat: add web project

---
+++ commenpatater-web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace commenpatater_web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
